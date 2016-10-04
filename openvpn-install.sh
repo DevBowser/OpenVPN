881f9a26cbeb0d85da64f6d16bc2bc8d49f4f117
Steven Wayne <DevBowser@users.noreply.github.com>
2016-10-03 18:03:36 -0700

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -2,8 +2,7 @@
 # OpenVPN road ez-mode installer for Debian, Ubuntu and CentOS
 
 # This script will work on Debian, Ubuntu, CentOS and probably other distros
-# of the same families, although no support is offered for them. It isn't
-# bulletproof but it will probably work if you simply want to setup a VPN on
+# of the same families, although no support is offered for them. It will probably work if you simply want to setup a VPN on
 # your Debian/Ubuntu/CentOS box. It has been designed to be as unobtrusive and
 # universal as possible.
 
